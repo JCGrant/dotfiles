43ba1c865d0d2c5084f464d25b6580b9178b87dc
JCGrant <jamescolin.grant@gmail.com>
2018-08-28 14:55:48 +0100

    Fix linking issue

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 dotfiles_path="$HOME/dotfiles"
 
-ln -sf $dotfiles_path/bash_profile ~/.bash_profile
-ln -sf $dotfiles_path/bashrc ~/.bashrc
-ln -sf $dotfiles_path/zshrc ~/.zshrc
+ln -sf "$dotfiles_path/bash_profile" ~/.bash_profile
+ln -sf "$dotfiles_path/bashrc" ~/.bashrc
+ln -sf "$dotfiles_path/zshrc" ~/.zshrc
 
-ln -sf $dotfiles_path/gitconfig ~/.gitconfig
+ln -sf "$dotfiles_path/gitconfig" ~/.gitconfig
 
-ln -sf $dotfiles_path/vimrc ~/.vimrc
+ln -sf "$dotfiles_path/vimrc" ~/.vimrc
 
-ln -sf $dotfiles_path/inputrc ~/.inputrc
+ln -sf "$dotfiles_path/inputrc" ~/.inputrc
 
-ln -sf $dotfiles_path/tmux.conf ~/.tmux.conf
+ln -sf "$dotfiles_path/tmux.conf" ~/.tmux.conf
 
 [ ! -d ~/.ssh ] && mkdir .ssh
-ln -sf $dotfiles_path/ssh/config ~/.ssh/config
+ln -sf "$dotfiles_path/ssh/config" ~/.ssh/config
 
 [ ! -x "$(command -v brew)" ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
